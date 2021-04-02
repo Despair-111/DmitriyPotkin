71c6f383daeec231cd2d0cb45dc440f06253ea1d
Despair-111 <dimapotkin14@gmail.com>
2021-04-02 15:01:45 +0300

    Исправлены замечания

--- Task 1-1.cpp
+++ Task 1-1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 /**
 * \brief Расчёт значения а
 * \param x Значение x
@@ -9,10 +10,12 @@ using namespace std;
 * \param z Значение z
 * \return Значение а
 */
+
 double getA(double x, double y, double z)
 {
 	return pow(x, y / x) - pow(y / x, 1 / 3.0); 
 }
+
 /**
 * \brief Расчёт значения b
 * \param x Значение x
@@ -20,29 +23,23 @@ double getA(double x, double y, double z)
 * \param z Значение z
 * \return Значение b
 */
+
 double getB(double x, double y, double z)
 {
 	return (y-x) + ((y-z)/(y-x))/(3 + (z*z/5));
 }
+
 int main()
 {
 	const auto x = 1.825;
 	const auto y = 18.225;
 	const auto z = -3.298;
+
     const auto a = getA(x, y, z);
 	const auto b = getB(x, y, z);
+
     cout << "x=" << x << ", y=" << y << ", z=" << z << "\n";
 	cout << "a=" << a << ", b=" << b;
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
