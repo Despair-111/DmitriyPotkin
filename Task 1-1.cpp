15b0e6ebe2429d6fbd918a24c64b2c794ff29820
Despair-111 <dimapotkin14@gmail.com>
2021-04-02 23:54:08 +0300

    Доработан код

--- Task 1-1.cpp
+++ Task 1-1.cpp
@@ -12,9 +12,6 @@ using namespace std;
 */
 
 double getA(double x, double y, double z)
-{
-	return pow(x, y / x) - pow(y / x, 1 / 3.0); 
-}
 
 /**
 * \brief Расчёт значения b
@@ -25,9 +22,6 @@ double getA(double x, double y, double z)
 */
 
 double getB(double x, double y, double z)
-{
-	return (y-x) + ((y-z)/(y-x))/(3 + (z*z/5));
-}
 
 int main()
 {
@@ -38,8 +32,18 @@ int main()
     const auto a = getA(x, y, z);
 	const auto b = getB(x, y, z);
 
-    cout << "x=" << x << ", y=" << y << ", z=" << z << "\n";
-	cout << "a=" << a << ", b=" << b;
+    cout << "x = " << x << ", y = " << y << ", z = " << z << "\n";
+	cout << "a = " << a << ", b = " << b;
 
     return 0;
+}
+
+double getA(double x, double y, double z)
+{
+	return pow(x, y / x) - pow(y / x, 1 / 3.0);
+}
+
+double getB(double x, double y, double z)
+{
+	return (y - x) + ((y - z) / (y - x)) / (3 + (z * z / 5));
 }
