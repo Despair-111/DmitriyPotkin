61e98e9f894f8904c27cb2ff56fceb28a585ca8e
Despair-111 <dimapotkin14@gmail.com>
2021-04-03 18:27:02 +0300

    1. Явно объявлен const
    2. Строки выравнены

--- Task 1-1.cpp
+++ Task 1-1.cpp
@@ -11,7 +11,7 @@ using namespace std;
 * \return Значение а
 */
 
-double getA(double x, double y, double z)
+double getA(const double x, const double y, const double z)
 
 /**
 * \brief Расчёт значения b
@@ -21,29 +21,29 @@ double getA(double x, double y, double z)
 * \return Значение b
 */
 
-double getB(double x, double y, double z)
+double getB(const double x, const double y, const double z)
 
 int main()
 {
-	const auto x = 1.825;
-	const auto y = 18.225;
-	const auto z = -3.298;
+const auto x = 1.825;
+const auto y = 18.225;
+const auto z = -3.298;
 
-    const auto a = getA(x, y, z);
-	const auto b = getB(x, y, z);
+const auto a = getA(x, y, z);
+const auto b = getB(x, y, z);
 
-    cout << "x = " << x << ", y = " << y << ", z = " << z << "\n";
-	cout << "a = " << a << ", b = " << b;
+cout << "x = " << x << ", y = " << y << ", z = " << z << "\n";
+cout << "a = " << a << ", b = " << b;
 
-    return 0;
+return 0;
 }
 
-double getA(double x, double y, double z)
+double getA(const double x, const double y, const double z)
 {
 	return pow(x, y / x) - pow(y / x, 1 / 3.0);
 }
 
-double getB(double x, double y, double z)
+double getB(const double x, const double y, const double z)
 {
 	return (y - x) + ((y - z) / (y - x)) / (3 + (z * z / 5));
 }
